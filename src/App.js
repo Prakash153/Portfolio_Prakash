1d098100e94402eca15394819d5399f385a8e4c0
Prakash15 <prakashkumarsingh483@gmail.com>
2023-03-19 00:02:20 +0530

    footer section completed

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
       <Work />
 
       <Skills />
-      <Testimonials />
+
       <Footer />
     </div>
   );
--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,26 +1,19 @@
+import React from "react";
 import { BsTwitter, BsInstagram } from "react-icons/bs";
 import { FaFacebookF } from "react-icons/fa";
 
-const SocialMedia = () => {
-  return (
-    <div className="app__social">
-      <div>
-        <a href="https://twitter.com/yourusername">
-          <BsTwitter />
-        </a>
-      </div>
-      <div>
-        <a href="https://www.instagram.com/_prakash151/">
-          <BsInstagram />
-        </a>
-      </div>
-      <div>
-        <a href="https://www.linkedin.com/in/yourusername/">
-          <FaFacebookF />
-        </a>
-      </div>
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+      <BsTwitter />
     </div>
-  );
-};
+    <div>
+      <FaFacebookF />
+    </div>
+    <div>
+      <BsInstagram />
+    </div>
+  </div>
+);
 
 export default SocialMedia;
