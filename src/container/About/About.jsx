7e87afc6652b3929ef5a707572b8056824fe0d09
Prakash15 <prakashkumarsingh483@gmail.com>
2023-03-20 03:02:27 +0530

    About section done

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
+import { color, motion } from "framer-motion";
 import "./About.scss";
 
 import { AppWrap, MotionWrap } from "../../wrapper";
@@ -17,28 +17,28 @@ const About = () => {
   }, []);
   return (
     <>
-      <h2 className=" head-text">
-        I Know that <span>Good Design</span> <br />
-        means <span>Good Business</span>
+      <h2 className=" head-text about-text">
+        Coding is the art of
+        <br />
+        turning <span>complexity</span> into <span>simplicity.</span>
       </h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
-          <motion.div
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: "tween" }}
-            className="app__profile-item"
-            key={about.title + index}
-          >
-            <img src={urlFor(about.imgUrl)} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>
-              {about.title}
-            </h2>
-            <p className="p-text" style={{ marginTop: 10 }}>
-              {about.description}
-            </p>
-          </motion.div>
+          <a href={about.profileLink} target="_blank" rel="noreferrer">
+            <motion.div
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.5, type: "tween" }}
+              className="app__profile-item"
+              key={about.title + index}
+            >
+              <img src={urlFor(about.imgUrl)} alt={about.title} />
+              <h2 className="app__profile-title" style={{ marginTop: 4 }}>
+                {about.title}
+              </h2>
+            </motion.div>
+          </a>
         ))}
       </div>
     </>
